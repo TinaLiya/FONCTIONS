39f9ea1a5c5745829f497aaeec473a02691c04db
jmoreau <julia.moreau@outlook.fr>
2014-12-20 23:24:38 +0100

    fini

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,20 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmoreau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/17 03:38:20 by jmoreau           #+#    #+#             */
-/*   Updated: 2014/12/20 22:32:28 by jmoreau          ###   ########.fr       */
+/*   Created: 2014/12/20 22:37:50 by jmoreau           #+#    #+#             */
+/*   Updated: 2014/12/20 23:18:02 by jmoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memccpy(void *dst, void *src, int c, size_t n)
 {
-	while (n--)
-		((char *)dst)[n] = ((char *)src)[n];
-	return (dst);
+		while (n--)
+		{
+			if (src[n] == c)
+				return (dst);
+			((char *)dst)[n] = ((char *)src)[n];
+		}
+		return (NULL);
 }
