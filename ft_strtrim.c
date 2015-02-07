f3b756364de6fe6ac91ef136da9938f8ae2907ea
jmoreau <julia.moreau@outlook.fr>
2015-02-07 21:09:15 +0100

    pfiouuu2

---
+++ ft_strtrim.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmoreau <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/07 20:32:33 by jmoreau           #+#    #+#             */
+/*   Updated: 2015/02/07 21:08:59 by jmoreau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strtrim(char const *s)
+{
+	char			*str;
+	int				i;
+	unsigned int	start;
+	size_t			len;
+
+	start = 0;
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s);
+	while (s[start] && ft_isspace(s[start]))
+		start++;
+	if (start == len)
+		return (NULL);
+	while (ft_isspace(s[len--]));
+	len = len - start;
+	str = ft_strsub(s, start, len);
+	return (str);
+}
