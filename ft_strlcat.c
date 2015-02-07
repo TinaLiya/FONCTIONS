66c89cf561f05a30f6fcbdb4be52a93de0dc7b8b
jmoreau <julia.moreau@outlook.fr>
2015-02-07 21:23:35 +0100

    hum

---
+++ ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmoreau <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/07 21:17:59 by jmoreau           #+#    #+#             */
+/*   Updated: 2015/02/07 21:23:19 by jmoreau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	len_dst;
+	size_t	len_src;
+	size_t	i;
+
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	i = 0;
+	if (size < len_dst)
+		return (len_src + size);
+	while (src[i] && len_dst + i + 1 < size)
+	{
+		dst[len_dst + i] = src[i];
+		i++;
+	}
+	dst[len_dst + 1] = '\0';
+	return (len_dst + len_src);
+}
