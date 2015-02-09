cf1842ee72da889e44580360f15a8fe604444b01
jmoreau <julia.moreau@outlook.fr>
2015-02-09 05:55:55 +0100

    verifie

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -1,18 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmoreau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/05 20:54:22 by jmoreau           #+#    #+#             */
-/*   Updated: 2015/02/09 05:48:28 by jmoreau          ###   ########.fr       */
+/*   Created: 2015/02/09 05:12:14 by jmoreau           #+#    #+#             */
+/*   Updated: 2015/02/09 05:55:29 by jmoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putchar(char c)
+static int		ft_strdiv(char *s, char c)
 {
-	write(1, &c, 1);
+	int	i;
+	int	k;
+
+	i = 0;
+	k = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] == c && s[i])
+		i++;
+	while (s[i])
+	{
+		k++;
+		while (s[i] != c && s[i])
+			i++;
+		while (s[i] == c && s[i])
+			i++;
+	}
+	return (k);
+}
+
+char			**ft_strsplit(char const*s, char c)
+{
+
 }
