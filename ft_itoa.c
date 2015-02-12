9ede6332067c054cd4c77be4b32adfe2833a5a04
jmoreau <julia.moreau@outlook.fr>
2015-02-12 01:06:13 +0100

    chier

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jmoreau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/10 03:27:37 by jmoreau           #+#    #+#             */
-/*   Updated: 2015/02/10 06:25:42 by jmoreau          ###   ########.fr       */
+/*   Updated: 2015/02/12 01:05:50 by jmoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ char	*ft_itoa(int n)
 
 	i = 0;
 	k = n;
+	if (n == 0)
+		return ("0");
+	if (n == -2147683647)
+		return ("-")
 	while (k)
 	{
 		k = k / 10;
@@ -29,6 +33,11 @@ char	*ft_itoa(int n)
 		i++;
 	str = ft_strnew(i + 1);
 	i--;
+	if (n < 0)
+	{
+		n = n * -1;
+		str[0] = '-';
+	}
 	while (n)
 	{
 		str[i] = (n % 10) + 48;
@@ -40,6 +49,6 @@ char	*ft_itoa(int n)
 
 int		main(void)
 {
-	ft_putstr(ft_itoa(-788009842));
+	ft_putstr(ft_itoa(-2147483647));
 	return (0);
 }
