7323d70c1bb67a658fdaf021eaeab6a4f7d4d9b0
jmoreau <julia.moreau@outlook.fr>
2015-02-14 08:29:43 +0100

    PUTAIN FINI

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: jmoreau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/07 20:12:35 by jmoreau           #+#    #+#             */
-/*   Updated: 2015/02/12 04:48:51 by jmoreau          ###   ########.fr       */
+/*   Updated: 2015/02/14 08:08:27 by jmoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 
 	i = 0;
 	str = ft_strnew(len + 1);
-	if (!s)
+	if (!s || len > ft_strlen(s))
 		return (NULL);
 	while (i != len)
 	{
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,29 +6,30 @@
 /*   By: jmoreau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/07 20:32:33 by jmoreau           #+#    #+#             */
-/*   Updated: 2015/02/12 04:49:53 by jmoreau          ###   ########.fr       */
+/*   Updated: 2015/02/14 08:29:15 by jmoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#include <stdio.h>
 char	*ft_strtrim(char const *s)
 {
 	char			*str;
-	unsigned int	start;
+	size_t			start;
 	size_t			len;
 
 	start = 0;
 	if (!s)
 		return (NULL);
-	len = ft_strlen(s);
+	len = ft_strlen(s) - 1;
 	while (s[start] && ft_isspace(s[start]))
 		start++;
-	if (start == len)
-		return (NULL);
+	if (start >= len)
+		return (ft_strdup(""));
 	while (ft_isspace(s[len]))
 		len--;
 	len = len - start;
-	str = ft_strsub(s, start, len);
+	str = ft_strsub(s, start, len + 1);
 	return (str);
 }
